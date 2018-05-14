b2232b6d95a7ec583cd880c930c7616eb144f2f8
madiyar.asetov <madiyar.asetov@gmail.com>
2018-05-14 10:45:02 +0600

    cs

--- src/app/construction-stage/construction-stage.component.ts
+++ src/app/construction-stage/construction-stage.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 interface ConstractionStageVM {
-  id: number;
+  id?: number;
   title: string;
   description: string;
+  squareMeterAmount: number;
+  flatAmount: number;
+  investmentAmount: number;
+  delayAmount: string;
+  doneForPercent: number;
+  pointAmount: number;
 }
 
 
@@ -16,7 +22,46 @@ export class ConstructionStageComponent implements OnInit {
 
   constructor() { }
 
+  public items: ConstractionStageVM[];
+
   ngOnInit() {
+
+    this.items =
+    [
+      {
+        id: 1,
+        title: 'г.Шымкент (МИО ЮКО)',
+        description: 'Строительство 9-ти этажного жилого дома по ул. Верхняя магитраль',
+        delayAmount: '+12 МЕС 20Д',
+        doneForPercent: 81,
+        investmentAmount: 3325000,
+        flatAmount: 275,
+        squareMeterAmount: 8258,
+        pointAmount: 11
+      },
+      {
+        id: 2,
+        title: 'г.Шымкент (МИО ЮКО)',
+        description: 'Строительство 9-ти этажного жилого дома по ул. Верхняя магитраль',
+        delayAmount: '+12 МЕС 20Д',
+        doneForPercent: 81,
+        investmentAmount: 3325000,
+        flatAmount: 275,
+        squareMeterAmount: 8258,
+        pointAmount: 11
+      },
+      {
+        id: 3,
+        title: 'г.Шымкент (МИО ЮКО)',
+        description: 'Строительство 9-ти этажного жилого дома по ул. Верхняя магитраль',
+        delayAmount: '+12 МЕС 20Д',
+        doneForPercent: 81,
+        investmentAmount: 3325000,
+        flatAmount: 275,
+        squareMeterAmount: 8258,
+        pointAmount: 11
+      }
+    ];
   }
 
 }
